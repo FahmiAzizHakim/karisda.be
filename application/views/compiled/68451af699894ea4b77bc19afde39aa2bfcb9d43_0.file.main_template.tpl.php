2aa902d9c4fafe17f7c558c11b6270fb56d0d96d
Munawar Holil <holilmunawar026@gmail.com>
2018-04-24 07:26:37 +0700

    update logo

--- application/views/compiled/68451af699894ea4b77bc19afde39aa2bfcb9d43_0.file.main_template.tpl.php
+++ application/views/compiled/68451af699894ea4b77bc19afde39aa2bfcb9d43_0.file.main_template.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-04-23 14:16:21
+/* Smarty version 3.1.30, created on 2018-04-23 14:19:13
   from "C:\xampp\htdocs\karisda.be\application\views\main_template.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5addce958dd2c7_08040725',
+  'unifunc' => 'content_5addcf4168e8d9_17669251',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '68451af699894ea4b77bc19afde39aa2bfcb9d43' => 
     array (
       0 => 'C:\\xampp\\htdocs\\karisda.be\\application\\views\\main_template.tpl',
-      1 => 1524485778,
+      1 => 1524485802,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5addce958dd2c7_08040725 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5addcf4168e8d9_17669251 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
